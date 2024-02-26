448616398e1af54312941239e7abbdcdb4346417
KwangInDaHo <khjin1197@gmail.com>
2024-02-27 01:16:15 +0900

    week2 - lotto : add LottoMain.kt with only money input and game generation

---
+++ khjin-tdd/src/main/kotlin/com/khjin/tdd_practice/week2/lotto/LottoMain.kt
@@ -0,0 +1,13 @@
+package com.khjin.tdd_practice.week2.lotto
+
+fun main() {
+    println("구매금액을 입력해 주세요")
+    val money = readln().toInt()
+
+    val lotto = Lotto()
+    val games = lotto.purchaseGames(LottoConstants.GAME_PRICE, money)
+    println("${games.size} 개를 구매했습니다")
+    for(game in games){
+        println(game)
+    }
+}
