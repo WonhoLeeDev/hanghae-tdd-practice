ff9d68e8a0e25fcf19376c898482bdecfda8cda3
WonhoLeeDev <wonholee.dev@gmail.com>
2024-03-02 23:57:45 +0900

    step2 - 로또번호 생성 재작성

--- src/main/java/tdd/practice/hanghae/lotto/step2/Lotto.java
+++ src/main/java/tdd/practice/hanghae/lotto/step2/Lotto.java
@@ -3,6 +3,8 @@ package tdd.practice.hanghae.lotto.step2;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
 
 public class Lotto {
     private final int LOTTO_PRICE = 1000;
@@ -24,18 +26,14 @@ public class Lotto {
     }
 
     public static List<Integer> getShuffledRandomLottoNumbers() {
-        Collections.shuffle(generateLottoNumbers());
-        List<Integer> randomLottoNumbers = generateLottoNumbers().subList(0, 6);
-        Collections.sort(randomLottoNumbers);
-        return randomLottoNumbers;
+        List<Integer> lottoNumbers = new ArrayList<>(generateLottoNumbers());
+        Collections.shuffle(lottoNumbers);
+        Collections.sort(lottoNumbers.subList(0, 6));
+        return lottoNumbers;
     }
 
-    public static List<Integer> generateLottoNumbers() {
-        List<Integer> lottoNumbers = new ArrayList<>();
-        for(int i=1; i<46; i++) {
-            lottoNumbers.add(i);
-        }
-        return lottoNumbers;
+    public static Set<Integer> generateLottoNumbers() {
+        return (Set<Integer>) Stream.iterate(0, n -> n + 1).limit(45);
     }
 
     public int getLottoBuyCountByBuyAmount(int buyAmount) {
