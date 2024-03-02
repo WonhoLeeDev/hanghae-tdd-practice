34201c8518df49fbc13a580e211f9354e58eb798
WonhoLeeDev <wonholee.dev@gmail.com>
2024-03-03 01:01:55 +0900

    step2 - 로또번호 생성 리팩토리중

--- src/main/java/tdd/practice/hanghae/lotto/step2/Lotto.java
+++ src/main/java/tdd/practice/hanghae/lotto/step2/Lotto.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Lotto {
@@ -18,22 +20,23 @@ public class Lotto {
 
     public List<List<Integer>> generateRandomLottoNumbers(int buyCount) {
         List<List<Integer>> boughtLottoList = new ArrayList<>();
-        for(int i = 0; i< buyCount; i++) {
-            List<Integer> randomLottoNumbers = getShuffledRandomLottoNumbers();
-            boughtLottoList.add(randomLottoNumbers);
-        }
+        IntStream.range(0, buyCount)
+                .mapToObj(i -> getShuffledRandomLottoNumbers())
+                .forEach(boughtLottoList::add);
+        System.out.println(boughtLottoList);
         return boughtLottoList;
     }
 
     public static List<Integer> getShuffledRandomLottoNumbers() {
-        List<Integer> lottoNumbers = new ArrayList<>(generateLottoNumbers());
-        Collections.shuffle(lottoNumbers);
-        Collections.sort(lottoNumbers.subList(0, 6));
-        return lottoNumbers;
+        List<Integer> generatedlottoNumbers = new ArrayList<>(generateLottoNumbers());
+        Collections.shuffle(generatedlottoNumbers);
+        List<Integer> shuffledLottoNumbers = generatedlottoNumbers.subList(0, 6);
+        Collections.sort(shuffledLottoNumbers);
+        return shuffledLottoNumbers;
     }
 
-    public static Set<Integer> generateLottoNumbers() {
-        return (Set<Integer>) Stream.iterate(0, n -> n + 1).limit(45);
+    public static List<Integer> generateLottoNumbers() {
+        return IntStream.rangeClosed(1, 45).boxed().collect(Collectors.toList());
     }
 
     public int getLottoBuyCountByBuyAmount(int buyAmount) {
