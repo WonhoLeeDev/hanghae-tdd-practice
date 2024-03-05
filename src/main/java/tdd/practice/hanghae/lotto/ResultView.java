a8406052341d3ae7316c7b1349d5ad8bc3f4b75c
WonhoLeeDev <wonholee.dev@gmail.com>
2024-03-05 14:27:35 +0900

    당첨결과 코드 리팩토링

--- src/main/java/tdd/practice/hanghae/lotto/ResultView.java
+++ src/main/java/tdd/practice/hanghae/lotto/ResultView.java
@@ -22,22 +22,27 @@ public class ResultView {
     }
 
     public void getWinningResult() {
+        printWinningResult(getMatchCount());
+    }
+
+    private void printWinningResult(int[] matchCount) {
         System.out.println("당첨 통계");
         System.out.println("--------");
-
-        int[] matchCount = new int[7];
-        List<List<Integer>> winningResult = this.lotto.getWinningResult();
-        for (List<Integer> winning : winningResult) {
-            int size = winning.size();
-            if (size >= 3 && size <= 6) {
-                matchCount[size]++;
-            }
-        }
         System.out.println("3개 일치 (" + (5000 * matchCount[3]) + "원)-" + matchCount[3] + "개");
         System.out.println("4개 일치 (" + (50000 * matchCount[4]) + "원)-" + matchCount[4] + "개");
         System.out.println("5개 일치 (" + (1500000 * matchCount[5]) + "원)-" + matchCount[5] + "개");
         System.out.println("6개 일치 (" + (2000000000 * matchCount[6]) + "원)-" + matchCount[6] + "개");
         int winningPrize = ( (5000 * matchCount[3]) + (50000 * matchCount[4]) + (1500000 * matchCount[5]) + (2000000000 * matchCount[6]) ) / this.lotto.getBuyAmount();
         System.out.println("총 수익률은" + winningPrize + "입니다.");
+    }
+
+    private int[] getMatchCount() {
+        int[] matchCount = new int[7];
+        List<List<Integer>> winningResult = this.lotto.getWinningResult();
+        winningResult.stream()
+                .map(List::size)
+                .filter(size -> size >= 3 && size <= 6)
+                .forEach(size -> matchCount[size]++);
+        return matchCount;
     }
 }
